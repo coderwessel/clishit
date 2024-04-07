ffcfd5ea1a51f9b22cc852cf4ac92eb4cd6ec38b
coderwessel <39902558+coderwessel@users.noreply.github.com>
2024-04-07 23:03:03 +0200

    cleanup

--- clishit/namecall2.py
+++ clishit/namecall2.py
@@ -4,6 +4,6 @@ from art import text2art
 @click.command()
 @click.option('--name', default='World', help='The person to greet.')
 def hello(name):
-   click.echo(text2art(f'Hello, {name}!'))
+   click.echo(text2art(f'Hello, {name}!!!'))
 if __name__ == '__main__':
    hello()
