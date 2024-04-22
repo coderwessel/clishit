4c080d6e4a7f3c09a2d84c1f3119a2121ae2d173
W.Kieft <w.kieft@uva.nl>
2024-04-22 20:14:00 +0200

    possible fix for cli not found error

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 setup(
    name='clishit',
-   version='0.4',
+   version='0.5',
    packages=find_packages(),
    install_requires=[
       'click',
    ],
    entry_points='''
       [console_scripts]
-      namecall2=clishit.cli.namecall2:hello
+      namecall2=cli.namecall2:hello
       ''',
 )
